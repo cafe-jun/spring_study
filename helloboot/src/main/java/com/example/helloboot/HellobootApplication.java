56658671038f236fc9d752ceef05e20dd9233455
cafe_jun <tb25271@gmail.com>
2025-11-20 22:11:34 +0900

    [feat] hello controller

---
+++ helloboot/src/main/java/com/example/helloboot/HellobootApplication.java
@@ -0,0 +1,62 @@
+package com.example.helloboot;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
+
+//@SpringBootApplication
+public class HellobootApplication {
+
+    public static void main(String[] args) {
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
+
+        ServletWebServerFactory factory = new TomcatServletWebServerFactory();
+
+        WebServer webServer = factory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
+                @Override
+                public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+                    System.out.println(req.getRequestURI() + " " + req.getMethod());
+                    if(req.getRequestURL().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+
+                        String name = req.getParameter("name");
+
+                        HelloController helloController1 = applicationContext.getBean(HelloController.class);
+
+                        String ret = helloController1.hello(name);
+
+                        res.setStatus(HttpStatus.OK.value());
+                        res.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                        res.getWriter().println(ret);
+
+
+                    }  else {
+                        res.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
+                }
+            }).addMapping("/*");
+        });
+        webServer.start();
+
+    }
+
+}
